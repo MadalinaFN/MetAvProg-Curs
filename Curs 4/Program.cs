fef4f8b627ff1c64ce7bdfb8c3c8376df03ccd2d
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-10-28 11:32:44 +0300

    LPO

---
+++ Curs 4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tube A = new Tube();
+            A.addbeg(1);
+            A.addbeg(2);
+            A.addbeg(3);
+            A.addbeg(4);
+            A.addend(5);
+            A.addend(6);
+            A.addend(7);
+            A.addend(8);
+            A.delend();
+            //A.view();
+
+            Stack B = new Stack();
+            B.Push(1);
+            B.Push(2);
+            B.Push(3);
+            B.Push(4);
+            B.Pop();
+            //B.view();
+
+            Queue C = new Queue();
+            C.Push(1);
+            C.Push(2);
+            C.Push(3);
+            C.Push(4);
+            C.Pop();
+            //C.view();
+
+            LPO lpo = new LPO();
+            lpo.Push(10);
+            lpo.Push(12);
+            lpo.Push(7);
+            lpo.Push(13);
+            lpo.Push(7);
+            lpo.Push(1);
+            lpo.Push(5);
+            lpo.Pop(7);
+            lpo.view();
+        }
+    }
+}
---
+++ Curs 4/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs_4
+{
+    public class Stack : Tube
+    {
+        public Stack():base()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            addbeg(x);
+        }
+
+        public int Pop()
+        {
+            return delbeg();
+        }
+
+        public override void view()
+        {
+            base.view();
+        }
+    }
+
+    public class Queue : Tube
+    {
+        public Queue() : base()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            addbeg(x);
+        }
+
+        public int Pop()
+        {
+            return delend();
+        }
+
+        public override void view()
+        {
+            base.view();
+        }
+    }
+}
